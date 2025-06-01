a77265c515e2bbc954646426a9bc97604c8e97f0
lihaonihao <lihaonihao333@github.com>
2025-06-01 22:47:49 +0800

    新增demo->hello

---
+++ demo/hello/app.py
@@ -0,0 +1,27 @@
+import click
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Hello, World!</h1>'
+
+@app.route('/hi')
+@app.route('/hello')
+def say_hello():
+    return '<h1>Hello, Flask!</h1>'
+
+@app.route('/greet', defaults={'name': 'Programmer'})
+@app.route('/greet/<name>')
+def greet(name):
+    return f'<h1>Hello, {name}!</h1>'
+
+@app.cli.command()
+def hello():
+    """Just say hello."""
+    click.echo('Hello, Human!')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
