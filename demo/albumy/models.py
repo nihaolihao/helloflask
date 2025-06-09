9f675e00a875cad162da6e08cd4b47f17518932a
lihaonihao <lihaonihao333@github.com>
2025-06-09 15:18:09 +0800

    修复sayhello

--- demo/albumy/models.py
+++ demo/albumy/models.py
@@ -32,6 +32,29 @@ class Role(db.Model):
                                  back_populates='roles')
     users = db.relationship('User', back_populates='role')
 
+    @staticmethod
+    def init_role():
+        roles_permissions_map = {
+            'Locked':['FOLLOW','COLLECT'],
+            'User':['FOLLOW','COLLECT','COMMENT','UPLOAD'],
+            'Moderator':['FOLLOW','COLLECT','COMMENT','UPLOAD','MODERATE'],
+            'Administrator':['FOLLOW','COLLECT','COMMENT','UPLOAD','MODERATE','ADMINISTER']
+            }
+    
+    for role_name in roles_permissions_map:
+        role = Role.query.filter_by(name=role_name).first()
+        if role is None:
+            role = Role(name=role_name)
+            db.session.add(role)
+        role.permissions = []
+        for permission_name in roles_permissions_map[role_name]:
+            permission = Permission.query.filter_by(name=permission_name).first()
+            if permission is None:
+                permission = Permission(name=permission_name)
+                db.session.add(permission)
+            role.permissions.append(permission)
+        db.session.commit()
+        
 class Permission(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30), unique=True)
